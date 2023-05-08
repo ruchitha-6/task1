86315aae7973d0eb90df040f295978d978703c16
ruchitha-6 <ruchia0708@gmail.com>
2023-05-08 16:14:23 +0530

    ludo

--- languages/javascript/ludo.js
+++ languages/javascript/ludo.js
@@ -16,15 +16,15 @@ winner.innerHTML="WINNER PLAYER-1"
 }
 
 function randomp2(){
-    let total2= document.getElementById('total')
+  let total2= document.getElementById('totalscore')
 
-   let num= Math.floor(Math.random()*6+1)
-  let ran= document.getElementById('randomscore')
-  ran.innerHTML=`${ num}`
-  totalp2.push(num)
-  let score=totalp2.reduce((a,b)=>a+b)
-  total2.innerHTML=score
-  if(score>=30){
-    winner.innerHTML="WINNER PLAYER-2"
-  }
+ let num= Math.floor(Math.random()*6+1)
+let ran= document.getElementById('randomscore')
+ran.innerHTML=`${ num}`
+totalp2.push(num)
+let score=totalp2.reduce((a,b)=>a+b)
+total2.innerHTML=score
+if(score>=30){
+winner.innerHTML="WINNER PLAYER-2"
+}
 }
